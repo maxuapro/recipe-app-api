330d44a1eb9ff636d9d7f0892905aabe85dd0b76
maxuapro <max.kosh.on@gmail.com>
2026-02-20 13:36:15 +0200

    section 14

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # recipe-app-api
+
+### I stopped at SECTION 14!!! Continue from THERE
+
 for Udemy Django rest API course
+
+after creating a model we do migration
+docker compose run --rm app sh -c "python manage.py makemigrations"
+
+after adding a new package
+we do docker-compose build
--- app/app/settings.py
+++ app/app/settings.py
@@ -38,10 +38,17 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'core'
+    'core',
+    'rest_framework',
+    'rest_framework.authtoken',
+    'drf_spectacular',
+    'user',
+    'recipe',
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -49,6 +56,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",        # local dev
 ]
 
 ROOT_URLCONF = 'app.urls'
@@ -128,3 +139,9 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'core.User'
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema'
+}
